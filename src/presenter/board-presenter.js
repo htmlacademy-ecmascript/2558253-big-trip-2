7b7a15d3ec6d376cbb7a4e64c8b3c8cbda011e0b
Bator Batuev <batorbatuev@gmail.com>
2025-01-19 00:51:58 +0800

    Время разбивать камни

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -4,7 +4,7 @@ import SortView from '../view/sort-view.js';
 import EventsListView from '../view/events-list-view.js';
 import FormEditView from '../view/form-edit-view.js';
 import PointView from '../view/point-view.js';
-import {render} from '../render.js';
+import {render, RenderPosition} from '../render.js';
 
 export default class BoardPresenter {
   sortComponent = new SortView();
@@ -21,6 +21,14 @@ export default class BoardPresenter {
 
     for (let i = 0; i < 3; i++) {
       render(new PointView(), this.eventsListComponent.getElement());
+    }
+    const headerContainer = document.querySelector('.trip-main');
+    if (headerContainer) {
+      render(new InfoTripView(), headerContainer, RenderPosition.AFTERBEGIN);
+    }
+    const filterContainer = document.querySelector('.trip-controls__filters');
+    if (filterContainer) {
+      render(new FilterView(), filterContainer);
     }
   }
 }
